623f94d5e8420936cc691e7f69b9bf19f748c43d
yamamotoyu <yu.yamamoto@rakus-partners.co.jp>
2021-05-17 12:22:07 +0900

    演習3完了

--- external_script.js
+++ external_script.js
@@ -4,6 +4,7 @@
 //演習2
 
 
+//交互に表示するためのclickToggle関数を作る。
 $.fn.clickToggle = function(a,b){
     return this.each(function(){
         var clicked = false;
@@ -17,12 +18,35 @@ $.fn.clickToggle = function(a,b){
 });
 };
 
+//ClickToggle関数を用いて交互にボタンを押した時の挙動をコーディング。
 $("#button").clickToggle(function(){
     $("#text").text("山本優");
     $("#text").css("color", "blue");
 }, function(){
     $("#text").css("color", "red");
 });
+
+//演習3
+
+$(function(){
+    /*selectボックスの洗濯が変更されたら中の処理を実行 */
+$("#interest-parent").on("change", function(){
+    /**v選択したselectボックスのvalue値を取得、valに代入 */
+    let val = $(this).val();
+    switch(val){
+        case "1":
+        $("#interest-child1").show();
+        $("#interest-child2").hide();
+        break;
+        case "2":
+        $("#interest-child1").hide();    
+        $("#interest-child2").show();
+    }
+        
+
+    });
+});
+
 
 
 
