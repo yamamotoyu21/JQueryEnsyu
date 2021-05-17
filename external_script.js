2378b892cbbdac744af47abc4966b80b92a53605
yamamotoyu <yu.yamamoto@rakus-partners.co.jp>
2021-05-17 11:50:43 +0900

    演習2完了

--- external_script.js
+++ external_script.js
@@ -1,17 +1,27 @@
 'use strict';
 
 
-//演習1
+//演習2
 
 
-
-$(function(){
-    $("#button").on("click",function(){
-        $("#text").text("山本優");
-      
-      
-       
+$.fn.clickToggle = function(a,b){
+    return this.each(function(){
+        var clicked = false;
+        $(this).on("click",function(){
+            clicked=!clicked;
+            if(clicked){
+                return a.apply(this,arguments);
+        }
+        return b.apply(this,arguments);
     });
+});
+};
+
+$("#button").clickToggle(function(){
+    $("#text").text("山本優");
+    $("#text").css("color", "blue");
+}, function(){
+    $("#text").css("color", "red");
 });
 
 
